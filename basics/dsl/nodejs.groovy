5a499a9d338dcb53b0e8c5dfebb265d4af5ee1b7
Tehilla Cohen <108400869+tehilla-cohen@users.noreply.github.com>
2022-10-19 12:38:53 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -16,4 +16,21 @@ job('NodeJS example') {
         shell("npm install")
     }
 }
-
+job('NodeJS example2') {
+    scm {
+        git('git://https://github.com/tehilla-cohen/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("docker run -it echo hello")
+    }
+}
